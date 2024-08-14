cd55957893d2865f433d37a2e7a74c302ee3eaaa
Tanvir Ahmad <tnvir9@gmail.com>
2024-08-14 09:34:22 +0600

    The Woman and Man jacket section is complete. All section is responsive for small, medium and large device.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        "custom-shadow": "0px 10px 30px 0px rgba(0, 0, 0, 0.05)",
+      },
+    },
   },
   plugins: [],
 };
